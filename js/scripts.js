db8000748d57aea584b264b37f65474e2f5e1d3c
Dawnlegend <104937387+Dawnlegend@users.noreply.github.com>
2022-05-25 20:07:26 -0700

    Added Mobile Menu Toggle
    - Added mobile menu toggle using JavaScript and added stylings with CSS for Exercise 2.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 console.log("Hello! Welcome to My Portfolio Site!")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    console.log("1")
+    x.className += ' responsive';
+  } else {
+    console.log("2")
+    x.className = 'navtoggle';
+  }
+}

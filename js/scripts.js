b30fd2ec1a03cff7ad09ce11b9667d2022996af2
Dawnlegend <104937387+Dawnlegend@users.noreply.github.com>
2022-06-02 22:38:20 -0700

    Corrected Errors
    - Corrected errors found using several linters for Exercise 2.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-console.log("Hello! Welcome to My Portfolio Site!")
+console.log('Hello! Welcome to My Portfolio Site!')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    console.log("1")
-    x.className += ' responsive';
+    console.log('1')
+    x.className += ' responsive'
   } else {
-    console.log("2")
-    x.className = 'navtoggle';
+    console.log('2')
+    x.className = 'navtoggle'
   }
 }
